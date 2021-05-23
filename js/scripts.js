3ea819b8b3ddb20c4a61adc17b869e259bc1d301
PaulaZP <mzp1405mail.com>
2021-05-23 17:19:38 -0600

    se modifica el codigo

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,50 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let xPosition = 20;
-let yPosition = 20;
-let xVelo = getRandomArbitrary(-5, 5);
-let yVelo = getRandomArbitrary(-5,5);
+//cuadrado
+let xPosition = 100;
+let yPosition = 100;
+let xVelo = getRandomArbitrary(-20, 20);
+let yVelo = getRandomArbitrary(-20,20);
 
+  //circulo
 let xPositionC = 50;
 let yPositionC = 50;
-let xVeloC = getRandomArbitrary(-5, 5);
-let yVeloC = getRandomArbitrary(-5,5);
+let xVeloC = getRandomArbitrary(-20, 20);
+let yVeloC = getRandomArbitrary(-20,20);
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     //cuadrado
     ctx.fillStyle = "blue";
-    ctx.fillRect(xPosition, yPosition, -10, -10);
+    ctx.fillRect(xPosition, yPosition, 100, 100);
 
-    //circulo
-    ctx.beginPath();
-    ctx.fillStyle = "red";
-    ctx.arc(xPositionC, yPositionC, 10, 0, 2 * Math.PI);
-    ctx.fill();
-
-    if(xPosition + xVelo > canvas.width || xPosition + xVelo < 0) {
+    if(xPosition + xVelo > canvas.width-100 || xPosition < 0) {
         xVelo = -xVelo;
     }
 
-    if(yPosition + yVelo > canvas.height || yPosition + yVelo < 0) {
+    if(yPosition + yVelo > canvas.height-100 || yPosition < 0) {
         yVelo = -yVelo;
-    }
-
-    if(xPositionC + xVeloC > canvas.width || xPositionC + xVeloC < 0) {
-        xVeloC = -xVeloC;
-    }
-
-    if(yPositionC + yVeloC > canvas.height || yPositionC + yVeloC < 0) {
-        yVeloC = -yVeloC;
     }
 
     xPosition += xVelo ;
     yPosition += yVelo ;
 
+
+    //circulo
+    ctx.beginPath();
+    ctx.fillStyle = "red";
+    ctx.arc(xPositionC, yPositionC, 50, 0, 2 * Math.PI);
+    ctx.fill();
+
+    if(xPositionC + xVeloC > canvas.width-50 || xPositionC + xVeloC < 50) {
+        xVeloC = -xVeloC;
+    }
+
+    if(yPositionC + yVeloC > canvas.height-50 || yPositionC + yVeloC < 50) {
+        yVeloC = -yVeloC;
+    }
     xPositionC += xVeloC ;
     yPositionC += yVeloC ;
     
